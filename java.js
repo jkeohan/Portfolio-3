32934d10a5ddad7f4243aa0edf6fbc2ea40b13f3
Gracie Everitt <gracieeveritt@Gracies-MacBook-Pro.local>
2020-03-09 17:54:28 -0500

    Monday commits

--- java.js
+++ java.js
@@ -1,1 +1,24 @@
-console.log('working')
+let source = "https://spreadsheets.google.com/feeds/list/1wVjdMce-oO0pND1evxCbDKfE0wYQf-CfBov_3DmnMuQ/od6/public/values?alt=json";
+
+fetch(source)
+  .then(response => response.json())
+  .then(data => {
+      console.log('data',data)
+      let projects = data.feed.entry.map(project =>{
+        console.log('project',project.gsx$title.$t)
+        return {
+          title: project.gsx$title.$t,
+          description: project.gsx$description.$t,
+          image: project.gsx$image.$t,
+          url: project.gsx$url.$t
+        }
+  })
+      console.log('projects',projects)
+      app(projects)
+})
+
+function app(projects){
+  console.log('app-projects',projects)
+  //my code goes here -- how to use this data from my project
+}
+
