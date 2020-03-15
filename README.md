da79d28dc70f81093db3470e71256c20adeb8663
Gracie Everitt <gracieeveritt@gracies-mbp.attlocal.net>
2020-03-15 14:53:28 -0500

    Final

--- README.md
+++ README.md
@@ -1,84 +1,76 @@
-##Live GitHub Page:
+## Live GitHub Page
 https://gracieeveritt.github.io/Portfolio/
+
+## YouTube Link
+https://youtu.be/0qAZwYf4UIc
 
 ## Project Schedule
 GitHub: https://github.com/GracieEveritt/Portfolio
 
 ## Project Description
-
 Online resume that provides visuals of my current experience with web development.
 
 ## Google Sheet
-
 List of Projects: https://docs.google.com/spreadsheets/d/1wVjdMce-oO0pND1evxCbDKfE0wYQf-CfBov_3DmnMuQ/edit#gid=0
 
 ## Wireframes
 
-Drawing of MVP Desktop: https://res.cloudinary.com/de2i4dwok/image/upload/v1583762889/Project%201/IMG_9056_idv4qv.jpg
+Drawing of MVP Desktop: https://res.cloudinary.com/de2i4dwok/image/upload/v1583762889/Project%201/IMG_9056_idv4qv.jpg  
+
 Drawing of Mobile & Tablet: https://res.cloudinary.com/de2i4dwok/image/upload/v1583780765/Project%201/IMG_9065_j4lg9e.jpg
 
 ## Time/Priority Matrix 
 
-|Deliverable	| Status	| Min.Spent |
+|Deliverable	| Status	| Time Spent |
 | --- | :---: |  :---: | 
-| Friday, Mar 6 | Research ideas & GIF fun	| Complete 	| 30 |
-| Sat, Mar 7 | Wireframes / Priority Matrix / Timeline	| Complete	| 90 |
-| Sun, Mar 8 | HTML built	| Complete | 30 |
-| Mon, Mar 9 | Compile List of Projects	| Complete | 45 |
-| Mon, Mar 9 | Implement Grid and Flexbox to Structure	 | In Progress | 145 |	
-| Mon, Mar 9 | Implement Tablet & Desktop Versions	 | In Progress | 140 |		
-| Tues, Mar 10 | Implement basic CSS Design | In Progress | 120 |		
-| Tues, Mar 10 | Create Contact Form	 | Complete | 60 |
-| Wed, Mar 11 | Create About Me	 | In Progress | 60 |		
-| Wed, Mar 11 | Implement Project Navigation with Jquery filter | Complete | 90 |	
-| Thurs, Mar 12 | Create Hamburger Header | In Progress  | 150 |		
-| Fri, Mar 13 | Link Contact to Google |  |
-| Create Hide Footer	 |  |  |	
-| Create page pop	 |  |  |		
-| Full page zoom & opacity to project cards	 |  |  |		
-| Side-bar navigation with icon images	 | In Progress | 30 |		
-| Hero image animation	 |  |  | 		
-| Hero image GIF animation	 |  |  |		
-| Finalize GitHub Documentation  |  |  |		
+| Friday, Mar 6 | Research ideas & GIF fun	| Complete 	| 1 hr |
+| Sat, Mar 7 | Wireframes / Priority Matrix / Timeline	| Complete	| 3 hrs |
+| Sun, Mar 8 | HTML built	| Complete | 30 min |
+| Mon, Mar 9 | Compile List of Projects	| Complete | 3 hrs |
+| Mon, Mar 9 | Research & Implement Grid and Flexbox to Structure w/ CSS	| Complete | 7.5 hrs |	
+| Mon, Mar 9 | Implement Tablet & Desktop Versions w CSS | Complete | 7 hrs |		
+| Tues, Mar 10 | Create Contact Form w Jquery	| Complete | 3 hrs |
+| Wed, Mar 11 | Create About Me	 | Complete | 2 hrs |		
+| Wed, Mar 11 | Implement Project Navigation with Jquery filter | Complete | 7 hrs |
+| Thurs, Mar 12 | Create Hamburger Header | Complete  | 3 hrs |		
+| Fri, Mar 13 | Side-bar navigation with icon images	 | Complete | 1 hr |		
+ Sun, Mar 15 | Finalize GitHub Documentation  | Complete | 2 hrs |		
 
 ## Functional Components
 
 | Component | Priority	| Estim Hrs |	Inves. Hrs	| Actual Hrs|
 | --- | :---: |  :---: | :---: | :---: |
-| Research |	H	| 0.5	| 0.5 |	0.5 |
+| Research |	H	| 0.5	| 1 |	1 |
 | Admin setup	| H	| 1	| 2	| 2 |
-| Wire drawing	| H |	0.5	| 0.75	| 0.5 |
-| GIF research	| H	 | 1	| 1.5 | 1.5 |
-| Research Grid Videos	| H	| 1	| 1.5	| 1.5 |
-| Compile List of Proj	| H	| 1	| 0.75 | 0.75 |
-| Implement Grid & Flex	| H	| 2	| 2.75 |  |
-| Implement Mobile Responsiveness	| H	| 2 | 2.5 |  |		
-| Create Contact Form	| H |	1 |  |  |	
-| Create About Me	| H |	1 |  |  |	
-| Proj Nav with jQuery Filter	| H |	3 |  |  |	
-| Hamburger	| H |	1 | 2.5 | 2.5 |	
+| Wire drawing	| H |	0.5	| 1	| 1 |
+| Research Grid & Flex 	| H	| 1	| 2	| 2 |
+| Compile List of Proj	| H	| 1	| 2 | 2 |
+| Implement Grid & Flex	| H	| 2	| 7 | 7 |
+| Implement Mobile Responsiveness	| H	| 2 | 7 | 7 |		
+| Create Contact Form w Jquery	| H |	1 | 3 | 3 |	
+| Create About Me	| H |	1 | 2 | 2 |	
+| Proj Nav with jQuery Filter	| H |	3 | 7 | 7 |	
+| Hamburger	| H |	1 | 3 | 3 |	
 | Hide Footer	| M |	1 |  |  |	
 | Scroll page pop	| M |	2 |  |  |		
 | Project Card Animation	| M |	2 |  |  |		
-| Side-bar navigation	| M |	2 | 40 | 40 |		
+| Side-bar navigation	| M |	2 | 1 | 1 |		
 | Hero Image animation | L |	2 |  |  |	
 | GIF Animation	| L |	6 |  |  |	
-| Finalize Git Hub Docs	| H |	1 |  |  |	
+| Finalize Video & Git Hub Docs	| H |	1 | 2 | 2 |	
 |				
-|TOTAL |	| 31	| 14.25 |	6.75 | -- will update upon the completion
-
-Google Spreadsheet: https://docs.google.com/spreadsheets/d/1mO2gktFiwwhvAyrH7yfoAn9F3tWQVB2KpXIfC5WphJ0/edit#gid=501344264
+|TOTAL |	| 30	| 40 |	40 |   
 
 ## Additional Libraries
-| *https://code.jquery.com/jquery-3.4.1.min.js"
-| *https://thenounproject.com/
-| Copyright: https://res.cloudinary.com/de2i4dwok/image/upload/v1583536372/Project%201/821011872_780x439_q0hxxi.jpg
-<script src="https://kit.fontawesome.com/1ec028bfe5.js" crossorigin="anonymous"></script>
-Form Builder: https://www.w3schools.com/howto/howto_css_contact_form.asp
-https://www.wiliam.com.au/wiliam-blog/jquery-and-css-media-queries
- 
+*https://code.jquery.com/jquery-3.4.1.min.js    
+*https://thenounproject.com/  
+*https://kit.fontawesome.com/1ec028bfe5.js  
+Form Builder: https://www.w3schools.com/howto/howto_css_contact_form.asp  
+https://www.wiliam.com.au/wiliam-blog/jquery-and-css-media-queries  
 
 ## Code Snippet
 
+https://res.cloudinary.com/de2i4dwok/image/upload/v1584299254/Project%201/Screen_Shot_2020-03-15_at_2.04.08_PM_eojgih.png
 
 ## Issues and Resolutions
+
--- java.js
+++ java.js
@@ -1,8 +1,6 @@
-$('#hamburger').click(function() {
-    $('.top_nav ul').css("flex-direction","column").css("align-items","flex-end")
-    
-    $('.top_nav ul li').toggle("fast","linear")
-    // $('.top_nav ul li').css('display',"block")
+$('#hamburger').click(function () {
+    $('.top_nav ul').css("flex-direction", "column").css("align-items", "flex-end")
+    $('.top_nav ul li').toggle("fast", "linear")
 })
 
 //Data Source -- GoogleSheet
@@ -11,55 +9,54 @@ let source = "https://spreadsheets.google.com/feeds/list/1wVjdMce-oO0pND1evxCbDK
 
 //"Fetch" function returns an Array "projects" of objects
 fetch(source)
-  .then(response => response.json())
-  .then(data => {
-      let projects = data.feed.entry.map(project =>{
-        return {
-          title: project.gsx$title.$t,
-          description: project.gsx$description.$t,
-          image: project.gsx$image.$t,
-          url: project.gsx$url.$t,
-          category: project.gsx$category.$t,
-          recent: project.gsx$recent.$t
-        }
-  })
-      app(projects)
-})
+    .then(response => response.json())
+    .then(data => {
+        let projects = data.feed.entry.map(project => {
+            return {
+                title: project.gsx$title.$t,
+                description: project.gsx$description.$t,
+                image: project.gsx$image.$t,
+                url: project.gsx$url.$t,
+                category: project.gsx$category.$t,
+                recent: project.gsx$recent.$t
+            }
+        })
+        app(projects)
+    })
 
+// Create subArrays using 2 functions app and loadCategory
 let jQuery = []
 let Flexbox_Grid = []
 let HTML_CSS = []
 let React = []
-let other = []
 
-function app(projects){    
-    for (i=0; i<projects.length; i++){
-        if (projects[i].category === "jQuery"){
-            jQuery.push(projects[i]) 
+function app(projects) {
+    for (i = 0; i < projects.length; i++) {
+        if (projects[i].category === "jQuery") {
+            jQuery.push(projects[i])
         }
-        else if (projects[i].category === "Flexbox_Grid"){
+        else if (projects[i].category === "Flexbox_Grid") {
             Flexbox_Grid.push(projects[i])
         }
-        else if (projects[i].category === "HTML_CSS"){
+        else if (projects[i].category === "HTML_CSS") {
             HTML_CSS.push(projects[i])
         }
         else {
             other.push(projects[i])
-        }        
+        }
     }
-    
     return loadCategory(jQuery)
 }
 
 const $project_parent = $('section.project_parent')
 
-function loadCategory(filteredArray){
-    for (i=0; i<filteredArray.length; i++){
+function loadCategory(filteredArray) {
+    for (i = 0; i < filteredArray.length; i++) {
         let $div = $('<div>').addClass('project')
         $project_parent.append($div)
         // let $h5 = $('<h5>').html(filteredArray[i].description).addClass('project_h5')
         // $div.append($h5)
-        let $img = $('<img>').attr('src',filteredArray[i].image).attr('alt',"project image").attr('title',`${filteredArray[i].description} example`).addClass('project_img')
+        let $img = $('<img>').attr('src', filteredArray[i].image).attr('alt', "project image").attr('title', `${filteredArray[i].description} example`).addClass('project_img')
         $div.append($img)
         let $h6 = $('<h6>').html(filteredArray[i].title).addClass('project_h6')
         $div.append($h6)
@@ -67,68 +64,48 @@ function loadCategory(filteredArray){
     return
 }
 
-
-
+// Event Listeners
 $('.li_flexbox').on('click', () => {
     $project_parent.empty()
-    $project_parent.css("background-color","rgba(194,198,78,1)")
-    $(".project_wrapper").css("background-color","rgba(194,198,78,1)")
+    $project_parent.css("background-color", "rgba(194,198,78,1)")
+    $(".project_wrapper").css("background-color", "rgba(194,198,78,1)")
     loadCategory(Flexbox_Grid)
-    $h6.css("background-color","inherit")
+    $h6.css("background-color", "inherit")
 })
 $('.li_html').on('click', () => {
     $project_parent.empty()
-    $project_parent.css("background-color","rgba(198,78,78,1)")
-    $(".project_wrapper").css("background-color","rgba(198,78,78,1)")
+    $project_parent.css("background-color", "rgba(198,78,78,1)")
+    $(".project_wrapper").css("background-color", "rgba(198,78,78,1)")
     loadCategory(HTML_CSS)
-    $h6.css("background-color","inherit")
+    $h6.css("background-color", "inherit")
 })
 $('.li_react').on('click', () => {
     $project_parent.empty()
-    $project_parent.css("background-color","rgba(78,130,198,1)").css("align-items","center")
-    $(".project_wrapper").css("background-color","rgba(78,130,198,1)")
+    $project_parent.css("background-color", "rgba(78,130,198,1)").css("align-items", "center")
+    $(".project_wrapper").css("background-color", "rgba(78,130,198,1)")
     let $div = $('<div>').addClass('project')
     $project_parent.append($div)
-    $div.css("height","225px").css("width","225px")
+    $div.css("height", "225px").css("width", "225px")
     let $h6 = $('<h6>').html(other[0].title).addClass('project_h6')
-        $div.append($h6)
-    $h6.css("background-color","inherit")
-    $(window).resize(function(){
+    $div.append($h6)
+    $h6.css("background-color", "inherit")
+    $(window).resize(function () {
         if ($('header').width() === "768px") {
-            $('.project_nav').css("max-width","175px")}
+            $('.project_nav').css("max-width", "175px")
+        }
     })
 })
 $('.li_jQuery').on('click', () => {
     $project_parent.empty()
-    $project_parent.css("background-color","rgba(80,150,146,1)")
-    $('.project_wrapper').css("background-color"," rgba(80,150,146,1)")
+    $project_parent.css("background-color", "rgba(80,150,146,1)")
+    $('.project_wrapper').css("background-color", " rgba(80,150,146,1)")
     loadCategory(jQuery)
-    $h6.css("background-color","inherit")
+    $h6.css("background-color", "inherit")
 })
 
+// Contact Form Listener
+$('#submit').on('click', () => {
+    alert("Thanks & Received!")
+    $('form')[0].reset()
 
-
-// {icon: "far fa-clock fa-2x", title: "Wedding Day Timeline", description:"The who, what, when and where of your wedding day in one timeline."}
-// ]
-// const $ul = $('<ul>')
-// const $nav = $('nav')
-// $nav.append($ul)
-
-// function htmlStructure(){
-//     for(i=0; i<$array.length; i++){
-//         let $li = $('<li>').addClass('ItemContainer')
-//         $ul.append($li)
-//         let $a = $('<a>').attr("href","#")
-//         $li.append($a)
-//         let $div_ImageContainer = $('<div>').addClass('ImageContainer')
-//         $a.append($div_ImageContainer)
-//         let $i = $('<i>').addClass($array[i].icon)
-//         $div_ImageContainer.append($i)
-//         let $div_ItemContentContainer = $('<div>').addClass('ItemContentContainer')
-//         $a.append($div_ItemContentContainer)
-//         let $h4 = $('<h4>').html($array[i].title) //can also use .text instead of .html//
-//         $div_ItemContentContainer.append($h4)
-//         ;
-//     }
-// } 
-// htmlStructure()
+})
